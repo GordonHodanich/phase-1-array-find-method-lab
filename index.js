e9b310b466c68e0c38f445c02fd27d1844fbf19c
Gordon Hodanich <ghodanich@gmail.com>
2021-05-25 13:53:09 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function superbowlWin(record){
+    function isWinner(element, index, array){
+        return (element.result === 'W')
+    }
+    let superbowlWin = record.find(isWinner)
+    if (typeof superbowlWin === "object"){
+        return superbowlWin.year
+    } else {
+        return undefined
+    }
+}
